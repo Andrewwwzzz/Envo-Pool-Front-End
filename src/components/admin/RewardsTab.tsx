257f637f42d224e802fa34cc69049b8c985846e3
zhong xian <zhongxian16@gmail.com>
2026-06-29 07:21:28 +0800

    Fix Rewards tab crash - correct RewardCatalogTab and MultiplierEventsTab imports

--- src/components/admin/RewardsTab.tsx
+++ src/components/admin/RewardsTab.tsx
@@ -13,7 +13,8 @@ import { fmtDateSG } from "@/lib/sgTime";
 import ReasonDialog from "./ReasonDialog";
 import DeletedBanner, { getDeletedInfo, isDeleted } from "./DeletedBanner";
 import { useAdminCustomers } from "@/hooks/useAdmin";
-import { MultiplierEventsTab, RewardCatalogTab } from "./MultiplierEventsTab";
+import { MultiplierEventsTab } from "./MultiplierEventsTab";
+import { RewardCatalogTab } from "./RewardCatalogTab";
 
 const TYPE_LABELS: Record<string, string> = {
   free_session: "Free Session",
